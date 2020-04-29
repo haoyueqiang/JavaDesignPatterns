2fa929a4b279a208b3f952999c0ea6249c96bf2f
haoyueqiang <17301744398@163.com>
2020-04-29 17:20:15 +0800

    1

---
+++ src/main/java/factoryPatterns/abstractFactory/LuxuryCarFactory.java
@@ -0,0 +1,18 @@
+package factoryPatterns.abstractFactory;
+
+
+public class LuxuryCarFactory implements CarFactory {
+
+	public Engine createEngine() {
+		return new LuxuryEngine();
+	}
+
+	public Seat createSeat() {
+		return new LuxurySeat();
+	}
+
+	public Tyre createTyre() {
+		return new LuxuryType();
+	}
+
+}
